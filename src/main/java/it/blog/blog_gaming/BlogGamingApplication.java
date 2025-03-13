e25accb0d28a9af3bffa76058259b40d9851d1d4
Mattia Tartaglia <mattia.tartaglia@outlook.it>
2025-03-13 16:10:27 +0100

    first commit

---
+++ src/main/java/it/blog/blog_gaming/BlogGamingApplication.java
@@ -0,0 +1,13 @@
+package it.blog.blog_gaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogGamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogGamingApplication.class, args);
+	}
+
+}
